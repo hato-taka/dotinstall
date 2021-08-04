c6b7225f700dd3d3c9ecf600149a5d0b29926afe
hato-taka <84255881+hato-taka@users.noreply.github.com>
2021-08-04 09:45:09 +0900

    commit js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,15 @@
 {
   const open = document.getElementById('open');
   const overlay =document.querySelector('.overlay');
+  const close = document.getElementById('close');
 
   open.addEventListener('click', ()=>{
     overlay.classList.add('active');
     open.classList.add('hide');
+  });
+  close.addEventListener('click', ()=>{
+    overlay.classList.remove('active');
+    open.classList.remove('hide');
   });
 
 }
