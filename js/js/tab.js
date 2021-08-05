5503db3ed2ca0eacb700fb2b92d2a1749a438512
hato-taka <84255881+hato-taka@users.noreply.github.com>
2021-08-05 23:56:10 +0900

    finish!
    very important last line

--- js/js/tab.js
+++ js/js/tab.js
@@ -1,6 +1,7 @@
 'use strict';
 {
   const menuItems = document.querySelectorAll('.menu li a');
+  const contents = document.querySelectorAll('.content');
 
   menuItems.forEach(clickedItem =>{
     clickedItem.addEventListener('click', e =>{
@@ -10,6 +11,11 @@
         item.classList.remove('active');
       });
       clickedItem.classList.add('active');
+
+      contents.forEach(content =>{
+        content.classList.remove('active');
+      });
+      document.getElementById(clickedItem.dataset.id).classList.add('active');
     });
   });
 }
