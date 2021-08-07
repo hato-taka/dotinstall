34039ef8c914f6954e7c2e0b0032b62b8c6bcacd
hato-taka <84255881+hato-taka@users.noreply.github.com>
2021-08-07 09:15:41 +0900

    #10
    addEventListener

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,10 @@
 'use strict';
 {
-  
+  const next = document.getElementById('next');
+  const prev = document.getElementById('prev');
+  const ul = document.querySelector('ul');
+
+  next.addEventListener('click', ()=>{
+    ul.style.transform = 'translateX(-200px)'
+  });
 }
