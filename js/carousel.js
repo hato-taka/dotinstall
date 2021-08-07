9918ef0d595aa941752fe87ae42d78c42509cc0a
hato-taka <84255881+hato-taka@users.noreply.github.com>
2021-08-07 09:36:26 +0900

    #12 important!
    point
     currentIndex--;

--- js/carousel.js
+++ js/carousel.js
@@ -4,9 +4,17 @@
   const prev = document.getElementById('prev');
   const ul = document.querySelector('ul');
   const slides = ul.children;
+  let currentIndex = 0;
 
   next.addEventListener('click', ()=>{
+    currentIndex++;
     const slideWidth = slides[0].getBoundingClientRect().width;
-    ul.style.transform = `translateX(${-1 * slideWidth}px)`
+    ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
   });
+
+prev.addEventListener('click', ()=>{
+  currentIndex--;
+  const slideWidth = slides[0].getBoundingClientRect().width;
+  ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
+});
 }
